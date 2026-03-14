eecb5751a98696df3e701e903111d5c77cfc4e11
Atthawut Smith <38793720+zachtix@users.noreply.github.com>
2026-03-15 00:10:31 +0700

    prepare data

--- data/profile.ts
+++ data/profile.ts
@@ -8,6 +8,7 @@ export const profile = {
     "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
   ABOUT_ME:
     "Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology. Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.",
+  IMAGES_ABOUT_ME: "https://github.com/shadcn.png",
   GITHUB: "https://github.com/shadcn",
   LINKEDIN: "https://www.linkedin.com/in/shadcn/",
   EMAIL: "shadcn@mail.com",
---
+++ data/project.ts
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Spotifu Music",
+    short_description:
+      "A music streaming app that emulates Spotify's core features.",
+    descriptions: [
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+    ],
+    image:
+      "https://plus.unsplash.com/premium_photo-1720029863009-3319eac8fcfc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG11c2ljJTIwYXBwfGVufDB8fDB8fHww",
+    preview_link: "https://google.com",
+    source: "https://github.com",
+    featured_project: true,
+  },
+  {
+    title: "Spotifu Music",
+    short_description:
+      "A music streaming app that emulates Spotify's core features.",
+    descriptions: [
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+    ],
+    image:
+      "https://plus.unsplash.com/premium_photo-1720029863009-3319eac8fcfc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG11c2ljJTIwYXBwfGVufDB8fDB8fHww",
+    preview_link: "https://google.com",
+    source: "https://github.com",
+    featured_project: false,
+  },
+  {
+    title: "Spotifu Music",
+    short_description:
+      "A music streaming app that emulates Spotify's core features.",
+    descriptions: [
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+      "A music streaming app that emulates Spotify's core features.",
+    ],
+    image:
+      "https://plus.unsplash.com/premium_photo-1720029863009-3319eac8fcfc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fG11c2ljJTIwYXBwfGVufDB8fDB8fHww",
+    preview_link: "https://google.com",
+    source: "https://github.com",
+    featured_project: true,
+  },
+]
+
+// Get featured projects from projects key:featured_project=true
+export const featured_projects = projects.filter(
+  (project) => project.featured_project
+)
